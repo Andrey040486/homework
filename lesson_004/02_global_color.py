5be992762d4ce9765220c7fd237d6fdfdf5b2eab
aik30 <satyriasis@mail.ru>
2021-11-23 13:30:18 +0300

    global_color complete

--- lesson_004/02_global_color.py
+++ lesson_004/02_global_color.py
@@ -1,13 +1,13 @@
 # -*- coding: utf-8 -*-
 import simple_draw as sd
 
+
 def figure(x, y, side, angle, length, color):
     point = sd.get_point(x, y)
     for i in range(side - 1):
-
         v1 = sd.get_vector(start_point=point, angle=angle, length=length, width=3)
         point = v1.end_point
-        angle += 360/side
+        angle += 360 / side
         v1.draw(color=color)
     sd.line(point, sd.get_point(x, y), color=color, width=3)
 
@@ -26,7 +26,7 @@ def figure(x, y, side, angle, length, color):
 # Результат решения см lesson_004/results/exercise_02_global_color.jpg
 
 colors = {'КРАСНЫЙ': sd.COLOR_RED, 'ОРАНЖЕВЫЙ': sd.COLOR_ORANGE, 'ЖЕЛТЫЙ': sd.COLOR_YELLOW, 'ЗЕЛЕНЫЙ': sd.COLOR_GREEN,
-                  'ГОЛУБОЙ': sd.COLOR_CYAN, 'СИНИЙ': sd.COLOR_BLUE, 'ФИОЛЕТОВЫЙ': sd.COLOR_PURPLE}
+          'ГОЛУБОЙ': sd.COLOR_CYAN, 'СИНИЙ': sd.COLOR_BLUE, 'ФИОЛЕТОВЫЙ': sd.COLOR_PURPLE}
 
 print('Возможные цвета:')
 for i in enumerate(colors):
@@ -35,15 +35,44 @@ for i in enumerate(colors):
 num_color = input('Введите желаемый цвет : ')
 df = int(num_color)
 if df == 0:
-    triangle = figure(100, 100, 3, 0, length=100, color=colors['КРАСНЫЙ'])
-    square = figure(400, 100, 4, 0, length=100, color=colors['КРАСНЫЙ'])
-    pentagon = figure(100, 400, 5, 0, length=100, color=colors['КРАСНЫЙ'])
-    hexagon = figure(400, 400, 6, 0, length=100, color=colors['КРАСНЫЙ'])
+    figure(100, 100, 3, 0, length=100, color=colors['КРАСНЫЙ'])
+    figure(400, 100, 4, 0, length=100, color=colors['КРАСНЫЙ'])
+    figure(100, 400, 5, 0, length=100, color=colors['КРАСНЫЙ'])
+    figure(400, 400, 6, 0, length=100, color=colors['КРАСНЫЙ'])
+elif df == 1:
+    figure(100, 100, 3, 0, length=100, color=colors['ОРАНЖЕВЫЙ'])
+    figure(400, 100, 4, 0, length=100, color=colors['ОРАНЖЕВЫЙ'])
+    figure(100, 400, 5, 0, length=100, color=colors['ОРАНЖЕВЫЙ'])
+    figure(400, 400, 6, 0, length=100, color=colors['ОРАНЖЕВЫЙ'])
+elif df == 2:
+    figure(100, 100, 3, 0, length=100, color=colors['ЖЕЛТЫЙ'])
+    figure(400, 100, 4, 0, length=100, color=colors['ЖЕЛТЫЙ'])
+    figure(100, 400, 5, 0, length=100, color=colors['ЖЕЛТЫЙ'])
+    figure(400, 400, 6, 0, length=100, color=colors['ЖЕЛТЫЙ'])
+elif df == 3:
+    figure(100, 100, 3, 0, length=100, color=colors['ЗЕЛЕНЫЙ'])
+    figure(400, 100, 4, 0, length=100, color=colors['ЗЕЛЕНЫЙ'])
+    figure(100, 400, 5, 0, length=100, color=colors['ЗЕЛЕНЫЙ'])
+    figure(400, 400, 6, 0, length=100, color=colors['ЗЕЛЕНЫЙ'])
+elif df == 4:
+    figure(100, 100, 3, 0, length=100, color=colors['ГОЛУБОЙ'])
+    figure(400, 100, 4, 0, length=100, color=colors['ГОЛУБОЙ'])
+    figure(100, 400, 5, 0, length=100, color=colors['ГОЛУБОЙ'])
+    figure(400, 400, 6, 0, length=100, color=colors['ГОЛУБОЙ'])
+elif df == 5:
+    figure(100, 100, 3, 0, length=100, color=colors['СИНИЙ'])
+    figure(400, 100, 4, 0, length=100, color=colors['СИНИЙ'])
+    figure(100, 400, 5, 0, length=100, color=colors['СИНИЙ'])
+    figure(400, 400, 6, 0, length=100, color=colors['СИНИЙ'])
+elif df == 6:
+    figure(100, 100, 3, 0, length=100, color=colors['ФИОЛЕТОВЫЙ'])
+    figure(400, 100, 4, 0, length=100, color=colors['ФИОЛЕТОВЫЙ'])
+    figure(100, 400, 5, 0, length=100, color=colors['ФИОЛЕТОВЫЙ'])
+    figure(400, 400, 6, 0, length=100, color=colors['ФИОЛЕТОВЫЙ'])
+
 
 else:
     print('Вы ввели некорректный номер!')
-
-
 
 # def figure(x, y, side, angle, length, color):
 #     point = sd.get_point(x, y)
@@ -59,9 +88,6 @@ else:
 # square = figure(400, 100, 4, 0,length=100)
 # pentagon = figure(100, 400, 5, 0,length=100)
 # hexagon = figure(400, 400, 6, 0,length=100)
-
-
-
 
 
 sd.pause()
