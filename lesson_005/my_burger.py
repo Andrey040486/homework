50ae5d40f00a70003e48e3a8e56c0ec2068bc156
aik30 <satyriasis@mail.ru>
2021-11-30 15:01:42 +0300

    les_5 burger

---
+++ lesson_005/my_burger.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+
+def buns():
+    print('А теперь добавим булочки')
+
+
+def cutlets():
+    print('А теперь добавим котлеты')
+
+
+def pickle():
+    print('А теперь добавим огурчика')
+
+
+def tomato():
+    print('А теперь добавим помидорчика')
+
+
+def mayonnaise():
+    print('А теперь добавим майонеза')
+
+
+def cheese():
+    print('А теперь добавим сыр')
