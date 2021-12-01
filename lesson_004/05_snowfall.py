5904ce5c4b376fc3edf96e29195d30daf19c84f6
aik30 <satyriasis@mail.ru>
2021-12-01 15:42:21 +0300

    snow to go

--- lesson_004/05_snowfall.py
+++ lesson_004/05_snowfall.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-
+from random import random, randint
 import simple_draw as sd
 sd.resolution = (1200, 600)
 
@@ -18,16 +18,16 @@ N = 20
 # sd.user_want_exit()
 
 
-while True:
-    sd.clear_screen()
-    pass
-    pass
-    pass
-    sd.sleep(0.1)
-    if sd.user_want_exit():
-        break
-
-sd.pause()
+# while True:
+#     sd.clear_screen()
+#     pass
+#     pass
+#     pass
+#     sd.sleep(0.1)
+#     if sd.user_want_exit():
+#         break
+#
+# sd.pause()
 
 # подсказка! для ускорения отрисовки можно
 #  - убрать clear_screen()
@@ -43,5 +43,28 @@ sd.pause()
 # - сделать сугоб внизу экрана - если снежинка долетает до низа, оставлять её там,
 #   и добавлять новую снежинку
 # Результат решения см https://youtu.be/XBx0JtxHiLg
+
+snow_x = (100, 150, 200, 250, 300, 350, 400, 450, 500, 550, 600, 650, 700, 750, 800, 850, 900, 950, 1000, 1050)
+y = 500
+snow_length = (1, 20, 2, 19, 3, 18, 4, 17, 5, 16, 6, 15, 7, 14, 8, 13, 9, 12, 10, 11)
+
+z = 0
+while True:
+    sd.clear_screen()
+    for i in range(N):
+
+        point = sd.get_point(snow_x[i] + z, y)
+        sd.snowflake(center=point, length=snow_length[i])
+        z += randint(-10, 10)
+
+    y -= 10
+    if y < 5:
+        break
+
+    sd.sleep(0.1)
+    if sd.user_want_exit():
+        break
+
+sd.pause()
 
 
--- probe.py
+++ probe.py
@@ -1,24 +1,28 @@
 # -*- coding: utf-8 -*-
+from random import random, randint
+
 import simple_draw as sd
 
 sd.resolution = (1200, 600)
 
 snow_x = (100, 150, 200, 250, 300, 350, 400, 450, 500, 550, 600, 650, 700, 750, 800, 850, 900, 950, 1000, 1050)
 y = 500
-snow_lenght = (1, 20, 2, 19, 3, 18, 4, 17, 5, 16, 6, 15, 7, 14, 8, 13, 9, 12, 10, 11)
+snow_length = (1, 20, 2, 19, 3, 18, 4, 17, 5, 16, 6, 15, 7, 14, 8, 13, 9, 12, 10, 11)
 
 # y = 500
 # x = 100
 #
 # y2 = 450
 # x2 = 150
-length = 1
+z = 0
 while True:
     sd.clear_screen()
-    for i in snow_x:
-        point = sd.get_point(i, y)
-        sd.snowflake(center=point, length=length)
-        length = sd.random_number(1, 10)
+    for i in range(20):
+
+        point = sd.get_point(snow_x[i] + z, y)
+        sd.snowflake(center=point, length=snow_length[i])
+        z += randint(-10, 10)
+
     y -= 10
     if y < 5:
         break
