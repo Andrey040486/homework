d887104cbba50c95dd004a699cf09b1e6f325c08
aik30 <satyriasis@mail.ru>
2021-12-01 16:06:15 +0300

    snow to go2

--- probe.py
+++ probe.py
@@ -9,36 +9,25 @@ snow_x = (100, 150, 200, 250, 300, 350, 400, 450, 500, 550, 600, 650, 700, 750, 
 y = 500
 snow_length = (1, 20, 2, 19, 3, 18, 4, 17, 5, 16, 6, 15, 7, 14, 8, 13, 9, 12, 10, 11)
 
-# y = 500
-# x = 100
-#
-# y2 = 450
-# x2 = 150
+
 z = 0
 while True:
-    sd.clear_screen()
+    sd.start_drawing()
     for i in range(20):
 
         point = sd.get_point(snow_x[i] + z, y)
         sd.snowflake(center=point, length=snow_length[i])
+        sd.snowflake(center=point, length=snow_length[i], color=sd.background_color)
         z += randint(-10, 10)
-
     y -= 10
     if y < 5:
         break
 
-
-    # point2 = sd.get_point(x2, y2)
-    # sd.snowflake(center=point2, length=30)
-    # y2 -= 10
-    # if y2 < 5:
-    #     break
-    # x2 = x2 + 20
-
+    sd.finish_drawing()
     sd.sleep(0.1)
     if sd.user_want_exit():
         break
-
+# sd.snowflake(center=point, length=snow_length[i], color=sd.background_color)
 sd.pause()
 
 
