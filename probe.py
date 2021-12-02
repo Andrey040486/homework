f1c26130913a579385aebf3bf34f7166b9fb8a02
aik30 <satyriasis@mail.ru>
2021-12-02 14:50:28 +0300

    snow_to_go_3

--- probe.py
+++ probe.py
@@ -4,30 +4,36 @@ from random import random, randint
 import simple_draw as sd
 
 sd.resolution = (1200, 600)
+def random_list(count, a, b):
+    list_ = []
+    for j in range(count):
+        list_.append(randint(a, b))
+    return list_
 
-snow_x = (100, 150, 200, 250, 300, 350, 400, 450, 500, 550, 600, 650, 700, 750, 800, 850, 900, 950, 1000, 1050)
+
+
 y = 500
-snow_length = (1, 20, 2, 19, 3, 18, 4, 17, 5, 16, 6, 15, 7, 14, 8, 13, 9, 12, 10, 11)
-
-
+snow_x = random_list(20, 90, 1060)
+snow_length = random_list(20, 1, 30)
 z = 0
 while True:
     sd.start_drawing()
     for i in range(20):
-
         point = sd.get_point(snow_x[i] + z, y)
-        sd.snowflake(center=point, length=snow_length[i])
         sd.snowflake(center=point, length=snow_length[i], color=sd.background_color)
-        z += randint(-10, 10)
     y -= 10
     if y < 5:
         break
+    z += randint(-10, 10)
+    for j in range(20):
+        point = sd.get_point(snow_x[j] + z, y)
+        sd.snowflake(center=point, length=snow_length[j], color=sd.COLOR_WHITE)
 
     sd.finish_drawing()
     sd.sleep(0.1)
     if sd.user_want_exit():
         break
-# sd.snowflake(center=point, length=snow_length[i], color=sd.background_color)
+
 sd.pause()
 
 
