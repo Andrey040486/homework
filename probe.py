91c98fcfddd6b18f7de93cc6bcd800ffdd8faf3a
aik30 <satyriasis@mail.ru>
2021-12-03 14:48:33 +0300

    wall bricks

--- probe.py
+++ probe.py
@@ -4,12 +4,13 @@ from random import random, randint
 import simple_draw as sd
 
 sd.resolution = (1200, 600)
+
+
 def random_list(count, a, b):
     list_ = []
     for j in range(count):
         list_.append(randint(a, b))
     return list_
-
 
 
 y = 500
@@ -35,27 +36,3 @@ while True:
         break
 
 sd.pause()
-
-
-# while True:
-#     sd.clear_screen()
-#     for i in range(50, 1000, 50):
-#         point = sd.get_point(i, y)
-#         sd.snowflake(center=point, length=10)
-#     y -= 10
-#     if y < 5:
-#         break
-#     i = i + 10
-#
-#     # point2 = sd.get_point(x2, y2)
-#     # sd.snowflake(center=point2, length=30)
-#     # y2 -= 10
-#     # if y2 < 5:
-#     #     break
-#     # x2 = x2 + 20
-#
-#     sd.sleep(0.1)
-#     if sd.user_want_exit():
-#         break
-#
-# sd.pause()
