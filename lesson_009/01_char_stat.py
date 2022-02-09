e5d8eb7ba4e3144942abcf83b4b872f0d9a13de7
aik30 <satyriasis@mail.ru>
2022-02-09 15:02:23 +0300

    упорядочил по алфавиту

--- lesson_009/01_char_stat.py
+++ lesson_009/01_char_stat.py
@@ -20,8 +20,57 @@
 #
 # Упорядочивание по частоте - по убыванию. Ширину таблицы подберите по своему вкусу
 # Требования к коду: он должен быть готовым к расширению функциональности. Делать сразу на классах.
+import os
+import zipfile
 
-# TODO здесь ваш код
+
+class Sum:
+    amount = 0
+
+    def __init__(self, file_name):
+        self.file_name = file_name
+        self.stat = {}
+
+    def unzip(self):
+        zfile = zipfile.ZipFile(self.file_name, 'r')
+        for filename in zfile.namelist():
+            zfile.extract(filename)
+        self.file_name = filename
+
+    def collect(self):
+        if self.file_name.endswith('.zip'):
+            self.unzip()
+        with open(self.file_name, 'r', encoding='cp1251') as file:
+            for line in file:
+                self._collect_for_line(line=line)
+
+    def _collect_for_line(self, line):
+        for char in line:
+            if char.isalpha():
+                if char in self.stat:
+                    self.stat[char] += 1
+                    self.amount += 1
+                else:
+                    self.stat[char] = 1
+                    self.amount += 1
+
+    def print(self):
+        print('+---------+----------+')
+        print('|  буква  | частота  |')
+        print('+---------+----------+')
+
+        for key, value in sorted(sum.stat.items()):
+            print(f'|{key: >5}    | {value: <8} |')
+        print('+---------+----------+')
+        print(f'| ИТОГО   | {self.amount}  |')
+        print('+---------+----------+')
+
+path = 'C:/Users/aik30/PycharmProjects/Python_base/lesson_009/python_snippets/voyna-i-mir.txt.zip'
+normalized_path = os.path.normpath(path)
+sum = Sum(file_name=normalized_path)
+
+sum.collect()
+sum.print()
 
 # После выполнения первого этапа нужно сделать упорядочивание статистики
 #  - по частоте по возрастанию
