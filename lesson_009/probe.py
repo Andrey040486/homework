92c3ca97b0265f3c4a7b50a9f601b6e2adb38f3b
aik30 <satyriasis@mail.ru>
2022-02-10 15:52:48 +0300

    парсер версия 1

--- lesson_009/probe.py
+++ lesson_009/probe.py
@@ -1,24 +1,51 @@
+# -*- coding: utf-8 -*-
+import os
 import zipfile
 
-from random import randint
 
-txt = 'строка'
-print(f'{txt:*^30}')
+class Parser:
+    pref_char = 0
+    step = 1
 
-# zip_file_name = 'voyna-i-mir.txt.zip'
-# zfile = zipfile.ZipFile(zip_file_name, 'r')
-# for filename in zfile.namelist():
-#     zfile.extract(filename)
-stat = {}
-next_char = 'а'
-file_name = 'voyna-i-mir.txt'
-with open(file_name, 'r', encoding='cp1251') as file:
-    for line in file:
-        for char in line:
-            if char in stat:
-                stat[char] += 1
-            else:
-                stat[char] = 1
-        else:
-            stat = {char: 1}
-print(stat)
+    def __init__(self, file_name):
+        self.file_name = file_name
+        # self.stat = {}
+
+    def unzip(self):
+        zfile = zipfile.ZipFile(self.file_name, 'r')
+        for filename in zfile.namelist():
+            zfile.extract(filename)
+        self.file_name = filename
+
+    def collect(self):
+        if self.file_name.endswith('.zip'):
+            self.unzip()
+        with open(self.file_name, 'r', encoding='cp1251') as file:
+            for line in file:
+                if 'NOK' in line:
+                    if line[16] == self.pref_char:
+                        self.step += 1
+                        # file = open('result.txt', mode='a')  # mode (режим): запись в конец
+                        # file_content = line
+                        # file.write(file_content)
+                    else:
+                        # self.step = 1
+                        self.pref_char = line[16]
+                    # print(self.step)
+                    # print('NOK найдена в строке', line)
+    #                 self.pars(line=line)
+                        print(f'{line[0:17]}] {self.step}')
+                        self.step = 1
+    #
+    # def pars(self, line):
+    #     for char in line:
+    #         print([16])
+
+            # if char[16] == self.pref_char:
+            #     # step += 1
+            #     file = open('result.txt', mode='a')  # mode (режим): запись в конец
+            #     file_content = line
+            #     file.write(file_content)
+            # self.pref_char = char[16]
+parser = Parser(file_name='events.txt')
+parser.collect()
