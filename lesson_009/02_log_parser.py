7c32d05a1635282fb87aca9cb596501887a4c23a
aik30 <satyriasis@mail.ru>
2022-02-14 10:26:59 +0300

    парсер готов

--- lesson_009/02_log_parser.py
+++ lesson_009/02_log_parser.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-
+import zipfile
 # Имеется файл events.txt вида:
 #
 # [2018-05-17 01:55:52.665804] NOK
@@ -19,7 +19,77 @@
 # Входные параметры: файл для анализа, файл результата
 # Требования к коду: он должен быть готовым к расширению функциональности. Делать сразу на классах.
 
-# TODO здесь ваш код
+class Parser:
+    pref_char = 0
+    step = 0
+
+    def __init__(self, file_name):
+        self.file_name = file_name
+
+    def unzip(self):
+        zfile = zipfile.ZipFile(self.file_name, 'r')
+        for filename in zfile.namelist():
+            zfile.extract(filename)
+        self.file_name = filename
+
+    def collect_min(self):
+        if self.file_name.endswith('.zip'):
+            self.unzip()
+        with open(self.file_name, 'r', encoding='cp1251') as file:
+            for line in file:
+                if 'NOK' in line:
+                    if line[0:17] == self.pref_char:
+                        self.step += 1
+                    elif self.pref_char == 0:
+                        self.step += 1
+                        self.pref_char = line[0:17]
+                    else:
+                        file = open('result_min.txt', mode='a')  # mode (режим): запись в конец
+                        file_content = f'\n{self.pref_char}] {self.step}'
+                        file.write(file_content)
+                        self.pref_char = line[0:17]
+                        self.step = 1
+
+    def collect_hour(self):
+        if self.file_name.endswith('.zip'):
+            self.unzip()
+        with open(self.file_name, 'r', encoding='cp1251') as file:
+            for line in file:
+                if 'NOK' in line:
+                    if line[0:14] == self.pref_char:
+                        self.step += 1
+                    elif self.pref_char == 0:
+                        self.step += 1
+                        self.pref_char = line[0:14]
+                    else:
+                        file = open('result_hour.txt', mode='a')  # mode (режим): запись в конец
+                        file_content = f'{self.pref_char}] {self.step}\n'
+                        file.write(file_content)
+                        self.pref_char = line[0:14]
+                        self.step = 1
+
+    def collect_month(self):
+        if self.file_name.endswith('.zip'):
+            self.unzip()
+        with open(self.file_name, 'r', encoding='cp1251') as file:
+            for line in file:
+                if 'NOK' in line:
+                    if line[0:8] == self.pref_char:
+                        self.step += 1
+                    elif self.pref_char == 0:
+                        self.step += 1
+                        self.pref_char = line[0:8]
+                    else:
+                        self.pref_char = line[0:8]
+                        self.step = 1
+            file = open('result_month.txt', mode='a')  # mode (режим): запись в конец
+            file_content = f'\n{self.pref_char}] {self.step}'
+            file.write(file_content)
+
+parser = Parser(file_name='events.txt')
+# parser.collect_min()
+# parser.collect_hour()
+# parser.collect_month()
 
 # После выполнения первого этапа нужно сделать группировку событий
 #  - по часам
--- lesson_009/probe.py
+++ lesson_009/probe.py
@@ -5,12 +5,10 @@ import zipfile
 
 class Parser:
     pref_char = 0
-    step = 1
-    z = 0
+    step = 0
 
     def __init__(self, file_name):
         self.file_name = file_name
-        # self.stat = {}
 
     def unzip(self):
         zfile = zipfile.ZipFile(self.file_name, 'r')
@@ -23,17 +21,17 @@ class Parser:
             self.unzip()
         with open(self.file_name, 'r', encoding='cp1251') as file:
             for line in file:
-                self.z += 1
                 if 'NOK' in line:
-                    if line[16] == self.pref_char:
+                    if line[0:17] == self.pref_char:
                         self.step += 1
-                        # file = open('result.txt', mode='a')  # mode (режим): запись в конец
-                        # file_content = line
-                        # file.write(file_content)
+                    elif self.pref_char == 0:
+                        self.step += 1
+                        self.pref_char = line[0:17]
                     else:
-                        self.pref_char = line[16]
-                        if self.z > 2:
-                            print(f'{line[0:17]}] {self.step}')
+                        file = open('result_min.txt', mode='a')  # mode (режим): запись в конец
+                        file_content = f'\n{self.pref_char}] {self.step}'
+                        file.write(file_content)
+                        self.pref_char = line[0:17]
                         self.step = 1
 
     def collect_hour(self):
@@ -41,19 +39,38 @@ class Parser:
             self.unzip()
         with open(self.file_name, 'r', encoding='cp1251') as file:
             for line in file:
-                self.z += 1
                 if 'NOK' in line:
-                    if line[12:14] == self.pref_char:
+                    if line[0:14] == self.pref_char:
                         self.step += 1
-                        # file = open('result.txt', mode='a')  # mode (режим): запись в конец
-                        # file_content = line
-                        # file.write(file_content)
+                    elif self.pref_char == 0:
+                        self.step += 1
+                        self.pref_char = line[0:14]
                     else:
-                        self.pref_char = line[12:14]
-                        if self.z > 2:
-                            print(f'{line[0:14]}] {self.step}')
+                        file = open('result_hour.txt', mode='a')  # mode (режим): запись в конец
+                        file_content = f'{self.pref_char}] {self.step}\n'
+                        file.write(file_content)
+                        self.pref_char = line[0:14]
                         self.step = 1
 
+    def collect_month(self):
+        if self.file_name.endswith('.zip'):
+            self.unzip()
+        with open(self.file_name, 'r', encoding='cp1251') as file:
+            for line in file:
+                if 'NOK' in line:
+                    if line[0:8] == self.pref_char:
+                        self.step += 1
+                    elif self.pref_char == 0:
+                        self.step += 1
+                        self.pref_char = line[0:8]
+                    else:
+                        self.pref_char = line[0:8]
+                        self.step = 1
+            file = open('result_month.txt', mode='a')  # mode (режим): запись в конец
+            file_content = f'\n{self.pref_char}] {self.step}'
+            file.write(file_content)
 
 parser = Parser(file_name='events.txt')
-parser.collect_min()
+# parser.collect_min()
+# parser.collect_hour()
+# parser.collect_month()
