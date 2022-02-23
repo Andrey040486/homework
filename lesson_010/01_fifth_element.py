4cd67425a68ff4d3fdb0d2ff4562492ef6b53029
Anna <aik303030@gmail.com>
2022-02-23 10:12:51 +0300

    пятый элемент готов

--- lesson_010/01_fifth_element.py
+++ lesson_010/01_fifth_element.py
@@ -4,10 +4,17 @@
 
 BRUCE_WILLIS = 42
 
-input_data = input('Если хочешь что-нибудь сделать, сделай это сам: ')
-leeloo = int(input_data[4])
-result = BRUCE_WILLIS * leeloo
-print(f"- Leeloo Dallas! Multi-pass № {result}!")
+try:
+    input_data = input('Если хочешь что-нибудь сделать, сделай это сам: ')
+    leeloo = int(input_data[4])
+    result = BRUCE_WILLIS * leeloo
+    print(f"- Leeloo Dallas! Multi-pass № {result}!")
+except IndexError:
+    print('Должно быть не менее 5 символов')
+except ValueError:
+    print('Введите число')
+except Exception:
+    print('Неизвестная ошибка')
 
 # Ообернуть код и обработать исключительные ситуации для произвольных входных параметров
 # - ValueError - невозможно преобразовать к числу
