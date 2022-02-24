e63a3ee03e455d8b2eabc6ae3c84d0b35c38f7e1
aik30 <satyriasis@mail.ru>
2022-02-24 09:47:34 +0300

    lesson 10.1

---
+++ lesson_010/probe.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+
+BRUCE_WILLIS = 42
+
+input_data = input('Если хочешь что-нибудь сделать, сделай это сам: ')
+leeloo = int(input_data[4])
+result = BRUCE_WILLIS * leeloo
+print(f"- Leeloo Dallas! Multi-pass № {result}!")
