0919f79bdfe6b62ba8153288138e56411d709676
aik30 <satyriasis@mail.ru>
2022-03-09 15:33:29 +0300

    prime_numbers v1.1

--- lesson_011/probe.py
+++ lesson_011/probe.py
@@ -1,32 +1,40 @@
-def get_prime_numbers(n):
-    prime_numbers = []
-    for number in range(2, n + 1):
-        for prime in prime_numbers:
-            if number % prime == 0:
-                break
-        else:
-            prime_numbers.append(number)
-    return prime_numbers
-
-
-numbers = get_prime_numbers(20)
-print(numbers)
+# def get_prime_numbers(n):
+#     prime_numbers = []
+#     for number in range(2, n + 1):
+#         for prime in prime_numbers:
+#             if number % prime == 0:
+#                 break
+#         else:
+#             prime_numbers.append(number)
+#     return prime_numbers
+#
+#
+# numbers = get_prime_numbers(20)
+# print(numbers)
 
 
 class PrimeNumbers:
 
     def __init__(self, n):
-        self.n, self.i = n, 0
+        self.n, self.i, self.prime_numbers = n, 0, []
 
     def __iter__(self):
-        self.i = 0
+        self.i = 1
         return self
 
     def __next__(self):
         self.i += 1
+        if self.i > self.n:
+            raise StopIteration()
+        else:
+            for prime in self.prime_numbers:
+                if self.i % prime == 0:
+                    break
+            else:
+                self.prime_numbers.append(self.i)
+                return self.i
 
 
-
-prime_number_iterator = PrimeNumbers(n=10000)
+prime_number_iterator = PrimeNumbers(n=10)
 for number in prime_number_iterator:
     print(number)
